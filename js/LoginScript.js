c5523dbd0236201fa28bd9420ec1d4a9ee3ffa31
Leonardodip <leonardodip@etevm.edu>
2018-09-04 20:02:38 -0300

    ponto ok commit inicial

--- js/LoginScript.js
+++ js/LoginScript.js
@@ -1,11 +1,12 @@
 
 $('form').submit(function (ev){
     ev.preventDefault();
-var email = $('inputEmail');
-var senha = $('inputSenha');
-
-if(senha.length>5){
-    
+var email = $('#inputEmail');
+var senha = $('#inputSenha');
+console.log(senha);
+if(senha.val().length>5){
+    console.log("entrou");
+    window.location.href="home/";
 }else{
     console.log("não entrou na condição")
     var no = document.createElement('div');
